fd814ddfaaa9c167133c007d0fc7ecd40cedbfd2
Sanapalli Lokesh <slokesh@akamai.com>
2020-11-22 11:26:46 +0530

    basic skeleton added'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import './styles.scss';
+import './styles.scss'
 
-document.getElementById('result').addEventListener('click', function handler() {
-    // eslint-disable-next-line no-alert
-    alert('click me!');
-});
+function generateRandomString() {
+    return 'adfaoij*242'
+}
+
+document
+    .getElementById('generatePassword')
+    .addEventListener('click', function handler() {
+        // generate password logic
+        const randomString = generateRandomString()
+        document.getElementById('result').innerHTML = randomString
+    })
