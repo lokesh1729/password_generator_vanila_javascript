7b624f99dd24e9ca38373ad8e7e9951e4e76688f
Sanapalli Lokesh <slokesh@akamai.com>
2020-11-23 10:47:08 +0530

    fix password duplication and added event handlers to symbols and number inputs

--- src/index.js
+++ src/index.js
@@ -59,12 +59,16 @@ const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const symbols = ['*', '@', '&', '%', '#', '$', '!'];
 
-function generateRandomString(choices, length) {
-    let result = '';
+function generateRandomString(choices, length, noDuplicates) {
+    const result = [];
     for (let index = 0; index < length; index += 1) {
-        result += choices[Math.floor(Math.random() * choices.length)];
+        let currRes = choices[Math.floor(Math.random() * choices.length)];
+        while (noDuplicates && result.includes(currRes)) {
+            currRes = choices[Math.floor(Math.random() * choices.length)];
+        }
+        result.push(currRes);
     }
-    return result;
+    return result.join('');
 }
 
 Math.shuffle = function shuffle(arr) {
@@ -80,11 +84,12 @@ Math.shuffle = function shuffle(arr) {
 };
 
 function generatePassword(maxLength, minSymbols, minNumbers) {
-    const symbolStr = generateRandomString(symbols, minSymbols);
-    const numStr = generateRandomString(numbers, minNumbers);
+    const symbolStr = generateRandomString(symbols, minSymbols, true);
+    const numStr = generateRandomString(numbers, minNumbers, true);
     const charStr = generateRandomString(
         alphabets,
-        maxLength - minNumbers - minSymbols
+        maxLength - minNumbers - minSymbols,
+        false
     );
     return Math.shuffle([
         ...symbolStr.split(''),
@@ -124,8 +129,49 @@ document
 document
     .getElementById('passwordLength')
     .addEventListener('change', function handler(event) {
-        const length = event.target.value;
+        const length = +event.target.value;
+        if (length > 25) {
+            // eslint-disable-next-line no-alert
+            alert('password should not exceed 25 in length');
+            return;
+        }
         const minSymbols = +document.getElementById('symbolLength').value;
+        const minNumbers = +document.getElementById('numberLength').value;
+        document.getElementById('result').value = generatePassword(
+            length,
+            minSymbols,
+            minNumbers
+        );
+    });
+
+document
+    .getElementById('numberLength')
+    .addEventListener('change', function handler(event) {
+        const minNumbers = +event.target.value;
+        if (minNumbers > 8) {
+            // eslint-disable-next-line no-alert
+            alert('numbers should not exceed 8 in password');
+            return;
+        }
+        const length = +document.getElementById('passwordLength').value;
+        const minSymbols = +document.getElementById('symbolLength').value;
+        document.getElementById('result').value = generatePassword(
+            length,
+            minSymbols,
+            minNumbers
+        );
+    });
+
+document
+    .getElementById('symbolLength')
+    .addEventListener('change', function handler(event) {
+        const minSymbols = +event.target.value;
+        if (minSymbols > 5) {
+            // eslint-disable-next-line no-alert
+            alert('symbols should not exceed 5 in password');
+            return;
+        }
+        const length = +document.getElementById('passwordLength').value;
         const minNumbers = +document.getElementById('numberLength').value;
         document.getElementById('result').value = generatePassword(
             length,
